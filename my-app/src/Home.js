fb4fc77772711494f71a6853a83184a0bded9eef
Jordan Darling <>
2022-04-26 23:57:11 -0400

    Working on personal page

--- my-app/src/Home.js
+++ my-app/src/Home.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import './css/Home.css';
+
+const HeroText = (props) => {
+    const [currentWord, wordSelect] = useState('');
+
+    useEffect(()=> {
+        let count = 0;
+        wordSelect(props.specialties[count]);
+        setInterval(function(){
+            count = (count >= props.specialties.length - 1) ? 0 : count+=1;
+            wordSelect(props.specialties[count]);
+        }, 2500); 
+    }, [])
+    return(
+        <span className="heroText">{currentWord}</span>
+    )
+}
 
 function Home(){
     return(
         <div>
             <h1>Hey there, I'm Jordan!</h1>
-            <h3>Welcome to my ReactJS practice site.</h3>
+            <h2>I'm a web developer in Rochester, New York, with knowledge in <HeroText specialties={['React', 'HTML5', 'CSS3', 'PHP']}/></h2>
             <div className="techList">
                 <h5>Tech used in this site</h5>
                 <ul>
                     <li>ReactJS</li>
                     <li>React Router</li>
-                    <li>Redux</li>
                 </ul>
             </div>
         </div>
