856922bf12e0e0a33209e7d06324b50f7d2232f2
Jordan Darling <26638275+jordandarling@users.noreply.github.com>
2022-05-25 11:55:10 -0400

    Added photo dropdown

---
+++ my-app/src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return(
+        <div>
+            <p>As you probably saw on the homepage, my name is Jordan, and I work for Rochester Institute of Technology. I started at RIT as an intern in May of 2019, and was eventually hird on as a full time Software Engineer.</p>
+            <p>In addition to my work, I have a variety of interests, such as photography, birdwatching, and playing video games. </p>
+        </div>
+    )
+}
+
+export default About;
--- my-app/src/Photography.js
+++ my-app/src/Photography.js
@@ -1,41 +1,70 @@
 import React from "react";
 import './css/Photography.css';
 
+
+const photos = [
+    {'image':'Moon', 'cameraSettings': 'Nikon D3500 | ISO 100 | &#119891;/11.0 | 1/100s<br/>300mm | 6.3 | 70.0-300.0 mm f/4.5-6.3<br/>5/15/2022 - 11:00PM', 'description': '<p>Shot during the lunar eclipse of May 2022, this image was my first attempt at any form of astrophotography. The moon is an excellent place to start with astrophotography, as it\'s bright, easy to focus on, and doesn\'t require long exposure times.</p><p>This was my favorite shot of the night, despite not displaying the \'blood moon\' of the evening. While not particularly crimson, this image is a clear display of the various craters and lines that cover the lunar surface.</p>'},
+    {'image':'Moon', 'cameraSettings': 'Nikon D3500 | ISO 100 | &#119891;/11.0 | 1/100s<br/>300mm | 6.3 | 70.0-300.0 mm f/4.5-6.3<br/>5/15/2022 - 11:00PM', 'description': '<p>Shot during the lunar eclipse of May 2022, this image was my first attempt at any form of astrophotography. The moon is an excellent place to start with astrophotography, as it\'s bright, easy to focus on, and doesn\'t require long exposure times.</p><p>This was my favorite shot of the night, despite not displaying the \'blood moon\' of the evening. While not particularly crimson, this image is a clear display of the various craters and lines that cover the lunar surface.</p>'}
+]
+
 const DisplayPhoto = (props) => {
+    console.log(props);
     return(
         <div>
             <figure>
-            <img src={require(`./images/photography/${props.photo}.JPG`)}/>
-            <figcaption></figcaption>
+            <img src={require(`./images/photography/${props.photo.image}.JPG`)} alt={props.photo.image}/>
+            <figcaption>
+                {props.photo.cameraSettings}
+            </figcaption>
             </figure>
-
+            {props.photo.description}
         </div>
+    )
+}
+
+/**
+ * Generate a select element populated with options from the photos array
+ * @param props  
+ * @returns Select element with options from photo array
+ */
+const ListPhotos = (props) => {
+    const photoList = props.photos.map((photo) =>
+        <option>{photo.image}</option>
+    );
+    return(
+        <select onChange={props.handleChange}>
+            {photoList}
+        </select>
     )
 }
 
 class Photography extends React.Component{
     constructor(props){
         super(props);
+        this.state = {
+            displayedPhoto: photos[0]
+        }
+        this.handleChange = this.handleChange.bind(this);
     }
-    
+
+    handleChange(event){
+        console.log(event.target.selectedIndex);
+        this.setState({
+            displayedPhoto: photos[event.target.selectedIndex]
+        })
+    }
+
     render(){
         return(
             <div className="photoGallery">
-                <h1>My Photo Gallery</h1>
-                <DisplayPhoto photo="Moon"/>
+                <div className="photoGalleryHeader">
+                    <h1>My Photo Gallery</h1>
+                    {<ListPhotos photos={photos} handleChange={this.handleChange}/>}
+                </div>
+                <DisplayPhoto photo={this.state.displayedPhoto} />
             </div>
         )
     }
 }
-
-
-let moon = {'image':'Moon', 'cameraSettings': 'Nikon D3500 | ISO 100 | &#119891;/11.0 | 1/100s<br/>300mm | 6.3 | 70.0-300.0 mm f/4.5-6.3<br/>5/15/2022 - 11:00PM', 'description': '<p>Shot during the lunar eclipse of May 2022, this image was my first attempt at any form of astrophotography. The moon is an excellent place to start with astrophotography, as it\'s bright, easy to focus on, and doesn\'t require long exposure times.</p><p>This was my favorite shot of the night, despite not displaying the \'blood moon\' of the evening. While not particularly crimson, this image is a clear display of the various craters and lines that cover the lunar surface.</p>'},
-gus = {'image':'Moon', 'cameraSettings': 'Nikon D3500 | ISO 100 | &#119891;/11.0 | 1/100s<br/>300mm | 6.3 | 70.0-300.0 mm f/4.5-6.3<br/>5/15/2022 - 11:00PM', 'description': '<p>Shot during the lunar eclipse of May 2022, this image was my first attempt at any form of astrophotography. The moon is an excellent place to start with astrophotography, as it\'s bright, easy to focus on, and doesn\'t require long exposure times.</p><p>This was my favorite shot of the night, despite not displaying the \'blood moon\' of the evening. While not particularly crimson, this image is a clear display of the various craters and lines that cover the lunar surface.</p>'}
-
-let photos = new Map();
-
-photos.set('moon', moon).set('gus', gus);
-
-console.log(photos);
 
 export default Photography;
